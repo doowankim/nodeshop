524f4a351b999f2b394e5e7f409744575a51af2e
Doowankim <rlaendhks11@gmail.com>
2019-09-15 11:42:53 +0900

    router part#2
    data CRUD

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,10 +1,42 @@
 const express = require('express');
 const router = express.Router();
 
+
+
+
+
+//read
 router.get('/',(req, res) =>{
    res.status(200).json({
        message: 'Successful Order'
    });
 });
+
+// create
+router.post('/',(req, res) =>{
+    res.status(200).json({
+        message: 'Successful post order'
+    });
+});
+
+
+// update
+router.patch('/',(req, res) => {
+   res.status(200).json({
+      msg: 'Successful patch order'
+   });
+});
+
+
+// delete
+router.delete('/',(req, res) => {
+    res.status(200).json({
+        msg: 'Successful delete order'
+    });
+});
+
+
+
+
 
 module.exports = router;
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+
+// read
 router.get('/', (req, res) => { //=>진행하겠다는 뜻
    res.status(200).json({
        message: 'Handling GET requests to /products'
    });
 });
+
+// create
+router.post('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Successful post products'
+    });
+});
+
+// update
+router.patch('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Successful patch products'
+    });
+});
+
+//delete
+router.delete('/', (req, res) => {
+    res.status(200).json({
+        msg: 'Successful delete products'
+    });
+});
+
 
 
 module.exports = router; //Router 기능을 내보내주는 것
