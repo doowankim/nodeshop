5c787c1c141461878a3d8ad21bd6d3d071600047
Doowankim <rlaendhks11@gmail.com>
2019-09-15 11:08:46 +0900

    create node server#2
    create server

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+
+
+app.use((req, res) => {
+  res.status(200).json({
+      message: 'It works!' //key: value (C: Create, R: Read, U: Update, D: Delete)를 하는 것
+  });
+});
+
+
+
+
+
 const PORT = 3000; //1000~9999
 const server = http.createServer(app);
 server.listen(PORT, console.log('serverstarted'));  //실행되는 코드
