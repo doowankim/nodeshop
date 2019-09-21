3e400982710f4be555f52e7ab3add98849beea7b
Doowankim <rlaendhks11@gmail.com>
2019-09-21 16:40:20 +0900

    add post order to mongodb

---
+++ api/models/order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const orderSchema = mongoose.Schema({
+    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true}, //type에는 mongoose의 할당된 id 값
+    quantity: { type: Number, default: 1} //default는 값을 넣지않으면 Value값이 1이 됨
+});
+
+module.exports = mongoose.model("order", orderSchema);
+
+
+
+
+
+// const mongoose = require('mongoose');
+//
+// //데이터 객체(schema:데이터 항목)
+// const productSchema = mongoose.Schema({
+//     name: {type: String, required: true}, //required는 필수 값
+//     price: {type: String, required: true}
+// });
+//
+// module.exports = mongoose.model("Product", productSchema); //model이 가장 최상위, mlab에서 뜨는건 products라고 뜸
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-
+const orderModel = require('../models/order'); //여기서 저장된 데이터는 ordermodel에 저장
+const productModel = require('../models/product');
 
 
 
@@ -14,14 +14,57 @@ router.get('/',(req, res) =>{
 
 // create
 router.post('/',(req, res) =>{
-    const order = {
-        name:req.body.product,
-        quenty:req.body.ea
-    };
-    res.status(200).json({
-        message: 'Successful post order',
-        createdOrder: order
-    });
+    productModel
+        .findById(req.body.product)
+        .then(product => {
+            if (!product) { //!는 부정의 뜻
+                return res.status(404).json({
+                    msg: 'Product not found'
+                });
+            }
+            else {
+                const order = new orderModel({
+                    product: req.body.product,
+                    quantity: req.body.quantity
+                });
+                return order.save();
+            }
+        })
+        .then(result => {
+            res.status(200).json({
+                msg: 'Successful post order',
+                createdOrder: {
+                    product: result.product,
+                    quantity: result.quantity,
+                    _id: result._id
+                }
+            });
+
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+
+    // const order = new orderModel({
+    //     product: req.body.product,
+    //     quantity: req.body.quantity
+    // });
+    // order
+    //     .save()
+    //     .then(result => {
+    //         res.status(200).json({
+    //             msg: 'Successful post order',
+    //             createdOrder: result
+    //         });
+    //
+    //     })
+    //     .catch(err => {
+    //         res.status(500).json({
+    //             error: err
+    //         });
+    //     });
 });
 
 
